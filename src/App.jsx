000edaa49050de07b03f81f1fd88a98b82e53a2c
Saurabh Singh <saurabh.rgt@gmail.com>
2023-10-23 20:01:20 +0530

    initial-commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,16 @@
+import { useState } from "react";
+import data from "./data";
+import List from "./list";
 const App = () => {
-  return <h2>Birthday Reminder - Starter</h2>;
+  const [people, setPeople] = useState(data);
+  console.log(people);
+  return (
+    <main>
+      <section className="container">
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+      </section>
+    </main>
+  );
 };
 export default App;
